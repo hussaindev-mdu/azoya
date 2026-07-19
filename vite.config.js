683533569f63656cbe447e5578be669d2f2de2d3
hussaindev-mdu <hussaindev07@gmail.com>
2026-07-20 03:43:10 +0530

    gh pages added

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs'
-import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'copy-404',
-      generateBundle() {
-        const source = path.resolve(__dirname, './public/404.html')
-        const dest = path.resolve(__dirname, './dist/404.html')
-        if (fs.existsSync(source)) {
-          fs.copyFileSync(source, dest)
-        }
-      },
-    },
-  ],
+  plugins: [react()],
   base: '/azoya/',
 })
