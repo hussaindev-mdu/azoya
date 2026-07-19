f7d04b52d0cc746ffa7f32225140ef21bd6dc174
hussaindev-mdu <hussaindev07@gmail.com>
2026-07-20 03:37:43 +0530

    images issue fixed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-404',
+      generateBundle() {
+        const source = path.resolve(__dirname, './public/404.html')
+        const dest = path.resolve(__dirname, './dist/404.html')
+        if (fs.existsSync(source)) {
+          fs.copyFileSync(source, dest)
+        }
+      },
+    },
+  ],
   base: '/azoya/',
 })
